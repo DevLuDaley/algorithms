cfaabdd582e30c99f08a7308236e07473f607d05
devludaley <daleyluis@gmail.com>
2021-05-06 13:48:05 -0400

    import and render <AlgoPage/>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
+import AlgoPage from './AlgoPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <AlgoPage/>
       </header>
     </div>
   );
