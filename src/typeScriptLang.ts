d53e83b135335e8aac2f5ccf0f842c8f8f20d07a
devludaley <daleyluis@gmail.com>
2021-09-24 19:36:02 -0400

    yarn add typescript + cleanup ts files

---
+++ src/typeScriptLang.ts
@@ -0,0 +1,16 @@
+// interface User {
+//   name: string;
+//   id: number;
+// }
+
+// let b = new User("Barry", 44)
+
+interface User {
+  name: string;
+  id: number;
+}
+ 
+const user: User = {
+  username: "Hayes",
+  id: 0,
+};
