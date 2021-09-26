08cf7db3bdfb3f40f72c90d388398f2d4eb341e6
devludaley <daleyluis@gmail.com>
2021-09-25 20:21:08 -0400

    add twoHighest Tests

---
+++ .history/src/twoHighest.test_20210925113742.js
@@ -0,0 +1,42 @@
+// function strCount(str, letter){
+const strCount = require("./allStarCodeChallenge18");
+test("Returns 10s", () => {
+    expect(strCount([4, 10, 10, 9])).toEqual([10,10]);
+    // expect(strCount('lllcdc', 'k')).toBe(0);
+});
+
+// test("Returns 3", () => {
+//     expect(strCount("Hellothere", "e")).toEqual(3);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+// test("Returns 1", () => {
+//     expect(strCount('fgakkiaekjbfcbikgigzkzk', "e")).toEqual(1);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+// test("hello 1", () => {
+//     expect(strCount('Hello', "o")).toEqual(1);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+// test("hello 2", () => {
+//     expect(strCount('Hello', "l")).toEqual(2);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+// test("hello 0", () => {
+//     expect(strCount('', "z")).toEqual(0);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+// test("helloll 4", () => {
+//     expect(strCount('helloll', "l")).toEqual(4);
+//     // expect(strCount('lllcdc', 'k')).toBe(0);
+// });
+
+
+
+// 'Hello', 'o', 1;
+// 'Hello', 'l', 2;
+// '', 'z', 0;
