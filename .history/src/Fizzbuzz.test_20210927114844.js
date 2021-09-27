2189ac12ee3b5b9a4a0c90c36b98e623b079a6d8
devludaley <daleyluis@gmail.com>
2021-09-27 15:38:12 -0400

    add tests for fizzbuzz.js

---
+++ .history/src/Fizzbuzz.test_20210927114844.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+// function strCount(str, letter){
+const fizzBuzz = require('./fizzBuzz');
+
+test('first', () => {
+  expect(fizzBuzz(15)).toEqual('fizzbuz');
+});
+
+// test('two', () => {
+//   expect(fizzBuzz([4, 10, 10, 9, 10])).toEqual([10, 9]);
+// });
+
+// test('Returns 15', () => {
+//   expect(fizzBuzz([15, 20, 20, 17])).toEqual([20, 17]);
+// });
