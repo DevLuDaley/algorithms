fb31a625361c9843cbb2f65185a5c5f5e530f1fc
devludaley <daleyluis@gmail.com>
2021-09-27 15:40:53 -0400

    create compGaming.js

---
+++ .history/src/HackerRank/compGaming_20210927143714.js
@@ -0,0 +1,15 @@
+/*
+ * Complete the 'numPlayers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY scores
+ */
+
+function numPlayers(k, scores) {
+  // Write your code here
+
+}
+
+module.exports(numPlayers);
