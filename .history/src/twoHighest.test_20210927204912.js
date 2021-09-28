581becf4820a3107ed00417f0f1dae90b248254c
devludaley <daleyluis@gmail.com>
2021-09-27 21:17:05 -0400

    add a new test to twoHighest.test.js

---
+++ .history/src/twoHighest.test_20210927204912.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+// function strCount(str, letter){
+const twoHighest = require('./twoHighest');
+
+test('first', () => {
+  expect(twoHighest([4, 10, 10, 9])).toEqual([10, 9]);
+});
+
+test('two', () => {
+  expect(twoHighest([10, 4, 10, 10, 9, 10, 10, 10])).toEqual([10, 9]);
+});
+
+test('Returns 17', () => {
+  expect(twoHighest([15, 20, 20, 17])).toEqual([20, 17]);
+});
+
+test('Returns 32 20', () => {
+  expect(twoHighest([15, 32, 20, 20, 17])).toEqual([32, 20, 17]);
+});
