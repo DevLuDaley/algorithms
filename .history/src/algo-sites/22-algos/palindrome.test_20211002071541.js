5e119a6968584d0d240ce021431434c9219b1548
devludaley <daleyluis@gmail.com>
2021-10-08 05:29:53 -0400

    solve && test palindrome

--- .history/src/algo-sites/22-algos/palindrome.test_20211002071541.js
+++ .history/src/algo-sites/22-algos/palindrome.test_20211002071541.js
@@ -5,10 +5,10 @@ describe('palindrome', () => {
     expect(palindrome('racecar')).toEqual(true);
   });
 
-  // it('Should return true', () => {
-  //   expect(palindrome('Cigar? Toss it in a can. It is so tragic')).toEqual(true);
-  //   //   assert.equal(isPalindrome("Cigar? Toss it in a can. It is so tragic"), true);
-  // });
+  it('Should return true', () => {
+    expect(palindrome('Cigar? Toss it in a can. It is so tragic')).toEqual(true);
+    //   assert.equal(isPalindrome("Cigar? Toss it in a can. It is so tragic"), true);
+  });
   it('Should return false', () => {
     expect(palindrome('sit ad est love')).toEqual(false);
   });
